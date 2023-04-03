311cc740a0e6c7802b67c7fe3d73a31f9b0fe004
Brendha <brendhasantanabss12@gmail.com>
2023-04-03 20:31:04 -0300

    Changes to be committed: 	new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   .styleci.yml 	new file:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/sanctum.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/lang/en/auth.php 	new file:   resources/lang/en/pagination.php 	new file:   resources/lang/en/passwords.php 	new file:   resources/lang/en/validation.php 	new file:   resources/views/logo.blade.php 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   server.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   webpack.mix.js

---
+++ routes/web.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Echo_;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{nome}', function ($nome) {
+    if  (strlen($nome) < 3  || (is_numeric($nome)))  {
+        echo 'Insira um nome válido';
+    }   
+    else{
+        echo 'Olá '.$nome .' seja bem-vindo ao meu site';
+    }
+})->name('nome');
+
+Route::get('/conta/{numero1}/{numero2}/{operacao?}', function ($numero1, $numero2, $operacao='') {
+    switch($operacao){  
+        case "soma":
+            $resultado = $numero1 + $numero2;
+            echo 'Soma : ' . $resultado;    
+            break;
+        case 'subtração':
+            $resultado = $numero1 - $numero2;
+            echo 'Subtração :' . $resultado;
+            break;
+        case 'multiplicação':
+            $resultado = $numero1 * $numero2;
+            echo 'Multiplicação: ' . $resultado;
+            break;
+        case 'divisão':
+            $resultado= $numero1 / $numero2;
+            echo 'Divisão: ' . $resultado;
+            break;  
+        case '':
+            $resultado = $numero1 + $numero2;
+            echo 'Soma: ' . $resultado;
+            $resultado = $numero1 - $numero2;
+            echo '</br>Subtração: ' . $resultado;
+            $resultado = $numero1 * $numero2;
+            echo '</br>Multiplicação: ' . $resultado;
+            $resultado = $numero1/$numero2;
+            echo '</br>Divisão: ' . $resultado;  
+            break;
+    };
+})->name('operations');
+//php artisan serve
+
+Route::get('/idade/{ano?}/{mês?}/{dia?}', function ($ano,$mês,$dia) {
+
+    $entrada = new DateTime(2022-04-01);
+    $saída= new DateTime($ano,$mês,$dia); 
+    $idade = $entrada->diff($saída);
+
+    echo $idade;
+
+
+
+
+
+    //if(empty($mês) || (empty($dia))){
+        //$idade = $entrada->diff($saída);
+        //echo $idade;
+    //}
+});
+
+
+
